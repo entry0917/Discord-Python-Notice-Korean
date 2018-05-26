6352201688b0a8d4e0fcb731f971bef8e9759ad0
kijk2869 <kijk2869@gmail.com>
2018-05-26 19:05:19 +0900

    메인프로그램 업데이트
    - 논란이 될수있는 주석을 순화시켰습니다.

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ maker = "351613953769603073"
 @app.event
 async def on_ready():
     print("Mary Notice Module for" , app.user.name, " (%s)" % app.user.id)
-    #매리의 ID 건들면 뒤짐
+    #매리의 ID 건들면 안됨
     owner.append("351613953769603073")
     owner.append(set.owner)
 # 메세지
